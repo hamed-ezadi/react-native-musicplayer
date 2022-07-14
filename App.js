8d3541839e92f63f3f4cd47633e2862f99254693
hamed-ezadi <hamedMishavian98@gmail.com>
2022-07-14 15:22:19 +0430

    native

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, StatusBar, StyleSheet} from 'react-native';
+import MusicPlayer from './screens/MusicPlayer';
+
+const App = () => {
+  return (
+    <View style={style.container}>
+      <StatusBar barStyle="light-content" />
+      <MusicPlayer />
+      heloword
+    </View>
+  );
+};
+
+export default App;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
